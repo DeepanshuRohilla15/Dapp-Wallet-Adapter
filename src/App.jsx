844c0581535be4e10a67d289b241b834a4bfe66d
Deepanshu Rohilla <20001001027deepanshu@dcrustm.org>
2024-10-25 21:15:55 +0530

    Add Send Tokens and Show Sol Balance features

--- src/App.jsx
+++ src/App.jsx
@@ -13,16 +13,20 @@ import { Airdrop } from './Airdrop';
 
 // Default styles that can be overridden by your app
 import '@solana/wallet-adapter-react-ui/styles.css';
+import { ShowSolBalance } from './ShowSolBalance';
+import { SendTokens } from './SendTokens';
 
 function App() {
   return (
     // endpoint -> replace it with devnet rpc
-    <ConnectionProvider endpoint={""}> 
+    <ConnectionProvider endpoint={"https://solana-devnet.g.alchemy.com/v2/gnKrq7uzcBIJ-8QfJq0qbfy9ki2zE_91"}> 
             <WalletProvider wallets={[]} autoConnect>
                 <WalletModalProvider>
                   <WalletMultiButton></WalletMultiButton>
                   <WalletDisconnectButton></WalletDisconnectButton>
                   <Airdrop />
+                  <ShowSolBalance />
+                  <SendTokens />
                 </WalletModalProvider>
             </WalletProvider>
         </ConnectionProvider>
